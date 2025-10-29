75516cd621796b880e738a7fb4de0e119399b239
Xoushal-from-Aprilia <christian.vedovi@isrosselliaprilia.edu.it>
2025-10-29 12:16:57 +0100

    update

---
+++ server.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHALYDfyXQIiEzc8IfH7aKjiyXNAWX4_0",
+  authDomain: "amicae-99c7b.firebaseapp.com",
+  projectId: "amicae-99c7b",
+  storageBucket: "amicae-99c7b.firebasestorage.app",
+  messagingSenderId: "379845487963",
+  appId: "1:379845487963:web:e73dc5c970e627676c7e7e",
+  measurementId: "G-LYV92RTCX7"
+};
